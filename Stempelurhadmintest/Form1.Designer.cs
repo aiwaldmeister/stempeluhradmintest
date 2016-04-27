9ee2a91841238e1510ea375e0e0bd5300428ef24
aiwaldmeister <schmidmichael1985@googlemail.com>
2016-04-27 17:19:51 +0200

    Tests mit Datagridview (füllen mit kalendertagen eines Monats)

--- Stempelurhadmintest/Form1.Designer.cs
+++ Stempelurhadmintest/Form1.Designer.cs
@@ -30,16 +30,27 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Montag = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dienstag = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mittwoch = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Donnerstag = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Freitag = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Samstag = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sonntag = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(30, 21);
+            this.button1.Location = new System.Drawing.Point(18, 551);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "init kalender";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // monthCalendar1
             // 
@@ -48,15 +59,96 @@
             this.monthCalendar1.ShowWeekNumbers = true;
             this.monthCalendar1.TabIndex = 1;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Montag,
+            this.Dienstag,
+            this.Mittwoch,
+            this.Donnerstag,
+            this.Freitag,
+            this.Samstag,
+            this.Sonntag});
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.WindowText;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 289);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(703, 242);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Montag
+            // 
+            this.Montag.Frozen = true;
+            this.Montag.HeaderText = "Montag";
+            this.Montag.Name = "Montag";
+            // 
+            // Dienstag
+            // 
+            this.Dienstag.Frozen = true;
+            this.Dienstag.HeaderText = "Dienstag";
+            this.Dienstag.Name = "Dienstag";
+            // 
+            // Mittwoch
+            // 
+            this.Mittwoch.Frozen = true;
+            this.Mittwoch.HeaderText = "Mittwoch";
+            this.Mittwoch.Name = "Mittwoch";
+            // 
+            // Donnerstag
+            // 
+            this.Donnerstag.Frozen = true;
+            this.Donnerstag.HeaderText = "Donnerstag";
+            this.Donnerstag.Name = "Donnerstag";
+            // 
+            // Freitag
+            // 
+            this.Freitag.Frozen = true;
+            this.Freitag.HeaderText = "Freitag";
+            this.Freitag.Name = "Freitag";
+            // 
+            // Samstag
+            // 
+            this.Samstag.Frozen = true;
+            this.Samstag.HeaderText = "Samstag";
+            this.Samstag.Name = "Samstag";
+            // 
+            // Sonntag
+            // 
+            this.Sonntag.Frozen = true;
+            this.Sonntag.HeaderText = "Sonntag";
+            this.Sonntag.Name = "Sonntag";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "MM.yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(280, 252);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(80, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(849, 606);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -65,6 +157,15 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Montag;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dienstag;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mittwoch;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Donnerstag;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Freitag;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Samstag;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sonntag;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
--- Stempelurhadmintest/Form1.cs
+++ Stempelurhadmintest/Form1.cs
@@ -16,5 +16,62 @@ namespace Stempelurhadmintest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            initCalendar(DateTime.Now.Month,DateTime.Now.Year);
+
+            //dataGridView1.RowCount = 5;
+            //dataGridView1.Rows[0].Cells[0].Value = "";
+            //dataGridView1.Rows[0].Cells[1].Value = "1";
+
+        }
+
+        private void initCalendar(int Betrachtungsmonat, int Betrachtungsjahr)
+        {
+            DateTime Betrachtungsdatum = new DateTime(Betrachtungsjahr,Betrachtungsmonat,1); //Startdate auf den ersten des Monats stellen
+            int Wochentagdesersten = ((int)Betrachtungsdatum.DayOfWeek == 0) ? 7 : (int)Betrachtungsdatum.DayOfWeek; //Nummer des Wochentags des ersten des Monats (Montag = 1, etc...)
+
+            dataGridView1.RowCount = 6;
+
+            int i = 1;
+            for (int actrow = 0; actrow < 6; actrow++)
+            {
+                for (int actcol = 0; actcol < 7; actcol++)
+                {
+                    if((actrow == 0 && actcol + 1 < Wochentagdesersten) || i > DateTime.DaysInMonth(Betrachtungsjahr,Betrachtungsmonat))
+                    {
+                        dataGridView1.Rows[actrow].Cells[actcol].Value = "";
+                        dataGridView1.Rows[actrow].Cells[actcol].Style.BackColor =Color.LightGray;
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[actrow].Cells[actcol].Value = i.ToString();
+                        i++;
+                    }
+
+
+                    
+                }
+            }
+
+
+
+
+            //DataGridViewRow myrow = new DataGridViewRow(); 
+
+            //for (int i = 0; i < Wochentagdesersten -1 ; i++)
+            //{
+            //    DataGridViewCell mycell = new DataGridViewTextBoxCell();
+            //    mycell.Value = "fill";
+            //    myrow.Cells.Add(mycell);
+
+            //}
+            //dataGridView1.Rows.Add(myrow);
+
+            //dataGridView1.Rows[0].Cells[2].Style.BackColor = Color.Aqua;
+
+            //MessageBox.Show(Wochentagdesersten.ToString());
+        }
     }
 }
