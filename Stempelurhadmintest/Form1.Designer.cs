bb95b32e17e570e3420911d68e0c4746e56d1ce1
aiwaldmeister <schmidmichael1985@googlemail.com>
2016-04-28 17:47:52 +0200

    Methoden zum Markieren von Ereignissen und zum Abarbeiten von selectierten Tagen vorbereitet.

--- Stempelurhadmintest/Form1.Designer.cs
+++ Stempelurhadmintest/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.Montag = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Dienstag = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Mittwoch = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -38,16 +37,16 @@
             this.Freitag = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Samstag = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Sonntag = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(220, 254);
+            this.button1.Location = new System.Drawing.Point(755, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(82, 29);
             this.button1.TabIndex = 0;
-            this.button1.Text = "init kalender";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -70,25 +69,14 @@
             this.Sonntag});
             this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
             this.dataGridView1.GridColor = System.Drawing.SystemColors.WindowText;
-            this.dataGridView1.Location = new System.Drawing.Point(18, 289);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 440);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 30;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.dataGridView1.Size = new System.Drawing.Size(283, 154);
             this.dataGridView1.TabIndex = 2;
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.CustomFormat = "MMMM yyyy";
-            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(19, 254);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.ShowUpDown = true;
-            this.dateTimePicker1.Size = new System.Drawing.Size(195, 26);
-            this.dateTimePicker1.TabIndex = 3;
             // 
             // Montag
             // 
@@ -146,6 +134,20 @@
             this.Sonntag.ReadOnly = true;
             this.Sonntag.Width = 40;
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.CustomFormat = "MMMM yyyy";
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 411);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(283, 26);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -156,6 +158,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
--- Stempelurhadmintest/Form1.cs
+++ Stempelurhadmintest/Form1.cs
@@ -19,17 +19,46 @@ namespace Stempelurhadmintest
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //initCalendar(DateTime.Now.Month,DateTime.Now.Year);
-            initCalendar(dateTimePicker1.Value.Month, dateTimePicker1.Value.Year);
-            
+            int selectedCellCount = dataGridView1.SelectedCells.Count;
+            for (int i = 0;
+                i < selectedCellCount; i++)
+            {
+                string cellvaluestring = dataGridView1.SelectedCells[i].Value.ToString();
+                cellvaluestring = Int32.Parse(cellvaluestring).ToString("D2");
 
-            //dataGridView1.RowCount = 5;
-            //dataGridView1.Rows[0].Cells[0].Value = "";
-            //dataGridView1.Rows[0].Cells[1].Value = "1";
-
+                if (cellvaluestring != "")
+                {
+                    MessageBox.Show( cellvaluestring + "." + dateTimePicker1.Value.Month.ToString("D2")+ "." + dateTimePicker1.Value.Year.ToString("D4"));
+                }
+            }
         }
 
-        private void initCalendar(int Betrachtungsmonat, int Betrachtungsjahr)
+        private void markiereBesondereTage()
+        {
+            string cellvaluestring = "";
+            string betrachtungsjahr = dateTimePicker1.Value.Year.ToString("D4");
+            string betrachtungsmonat = dateTimePicker1.Value.Month.ToString("D2");
+
+            for (int actrow = 0; actrow < 6; actrow++)
+            {
+                for (int actcol = 0; actcol < 7; actcol++)
+                {
+                    cellvaluestring = dataGridView1.Rows[actrow].Cells[actcol].Value.ToString();
+                    cellvaluestring = Int32.Parse(cellvaluestring).ToString("D2");
+                    if (cellvaluestring != "")
+                    {   //select auf kalenderdatenbank mit betrachtungsjahr, betrachtungsmonat, betrachtungstag(cellvaluestring)
+                        //wenn ereignis für diesen Tag gefunden, die cell einfärben und den tooltip setzen
+                        
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+            }
+        }
+
+        private void initKalender(int Betrachtungsmonat, int Betrachtungsjahr)
         {
             DateTime Betrachtungsdatum = new DateTime(Betrachtungsjahr,Betrachtungsmonat,1); //Startdate auf den ersten des Monats stellen
             int Wochentagdesersten = ((int)Betrachtungsdatum.DayOfWeek == 0) ? 7 : (int)Betrachtungsdatum.DayOfWeek; //Nummer des Wochentags des ersten des Monats (Montag = 1, etc...)
@@ -45,7 +74,7 @@ namespace Stempelurhadmintest
                     if((actrow == 0 && actcol + 1 < Wochentagdesersten) || i > DateTime.DaysInMonth(Betrachtungsjahr,Betrachtungsmonat))
                     {
                         dataGridView1.Rows[actrow].Cells[actcol].Value = "";
-                        dataGridView1.Rows[actrow].Cells[actcol].Style.BackColor =Color.LightGray;
+                        dataGridView1.Rows[actrow].Cells[actcol].Style.BackColor = Color.LightGray;
                     }
                     else
                     {
@@ -54,9 +83,6 @@ namespace Stempelurhadmintest
                         dataGridView1.Rows[actrow].Cells[actcol].ToolTipText = "test\r\ntest";
                         i++;
                     }
-
-
-                    
                 }
             }
 
@@ -77,6 +103,16 @@ namespace Stempelurhadmintest
             //dataGridView1.Rows[0].Cells[2].Style.BackColor = Color.Aqua;
 
             //MessageBox.Show(Wochentagdesersten.ToString());
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            initKalender(dateTimePicker1.Value.Month, dateTimePicker1.Value.Year);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initKalender(dateTimePicker1.Value.Month, dateTimePicker1.Value.Year);
         }
     }
 }
